1175402ac664680c5b4d43ddda749647dc4b7db7
Ilya Belov <iluhabelov2255@gmail.com>
2023-12-19 16:23:55 +0300

    Test

--- new.py
+++ new.py
@@ -1,4 +1,20 @@
-def make_user(id, live, options):
-    return {'id': id, 'live': live, 'options': options}
+def likes(lst):
+    if len(lst) == 0:
+        return 'Никто не оценил данную запись'
+    elif len(lst) == 1:
+        return f'{lst[0]} оценил(а) данную запись'
+    elif len(lst) == 2:
+        return f'{lst[0]} и {lst[1]} оценили данную запись'
+    elif len(lst) == 3:
+        return f'{lst[0]}, {lst[1]} и {lst[2]} оценили данную запись'
+    else:
+        return f'{lst[0]}, {lst[1]} и {len(lst) - 2} других оценили данную запись'
 
-print(make_user(4, False, None))
+
+print(likes([]))
+print(likes(['Тимур']))
+print(likes(['Тимур', 'Артур']))
+print(likes(['Тимур', 'Артур', 'Руслан']))
+print(likes(['Тимур', 'Артур', 'Руслан', 'Анри']))
+print(likes(['Тимур', 'Артур', 'Руслан', 'Анри', 'Дима']))
+print(likes(['Тимур', 'Артур', 'Руслан', 'Анри', 'Дима', 'Рома', 'Гвидо', 'Марк']))
