6a4a3e816112de3c6b0c8dbb0f0d9c17dc435a93
Ilya Belov <154321709+MrHerrington@users.noreply.github.com>
2024-09-27 22:04:14 +0300

    Solving exercism portal bash tasks.

---
+++ bash/error_handling.sh
@@ -0,0 +1,27 @@
+: "
+Instructions
+
+Implement various kinds of error handling and resource management.
+An important point of programming is how to handle errors and close resources even if errors occur.
+This exercise requires you to handle various errors. Because error handling is rather programming language
+specific you'll have to refer to the tests for your track to see what's exactly required.
+
+Bash-specific instructions
+
+The goal of this exercise is to consider the number of arguments passed to your program. If there is exactly
+one argument, print a greeting message. Otherwise print an error message and exit with a non-zero status.
+"
+
+
+#!/usr/bin/env bash
+
+main () {
+    if [[ "$#" == 1 ]]; then
+        echo "Hello, $1"
+    else
+        echo "Usage: error_handling.sh <person>"
+        exit 1
+    fi
+}
+
+main
---
+++ bash/hello_world.sh
@@ -0,0 +1,14 @@
+: "
+Instructions
+
+The classical introductory exercise. Just say 'Hello, World!'.
+"
+
+
+#!/usr/bin/env bash
+
+main () {
+  echo "Hello, World!"
+}
+
+main "$@"
